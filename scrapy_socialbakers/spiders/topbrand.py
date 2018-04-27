6540dee6858c6cc3af3469ddd907f7230427717d
Sy Hung NGHIEM <cappuccino2806@gmail.com>
2018-04-27 22:00:10 +0200

    Get HashTag
    crawl hashtag

--- scrapy_socialbakers/spiders/topbrand.py
+++ scrapy_socialbakers/spiders/topbrand.py
@@ -7,7 +7,10 @@ class TopbrandSpider(scrapy.Spider):
 
     def __init__(self, country='france', *args, **kwargs):
         super(TopbrandSpider, self).__init__(*args, **kwargs)
-        self.start_urls = ['http://www.socialbakers.com/statistics/facebook/pages/total/%s/brands/page-1-5/' % country]
+        brand = getattr(self, 'brand', '')
+        if brand:
+            brand= '/'+brand
+        self.start_urls = ['http://www.socialbakers.com/statistics/facebook/pages/total/%s/brands%s/page-1-5/' %(country,brand)]
 
     def parse(self, response):
         for brand in response.css("table.brand-table-list tr"):
@@ -19,11 +22,22 @@ class TopbrandSpider(scrapy.Spider):
     def parse_brand(self,response):
     	pos=response.meta['pos']
     	brand_name=response.meta['brand_name']
+    	fans=response.css('.account-detail span.p-nw strong::text').extract_first().strip()
     	img_url=response.css('.account-detail div.img img::attr(src)').extract()
+    	tagLinks=response.css('.account-detail .account-tag-list li a')
+    	tags=[]
+    	for link in enumerate(tagLinks):
+    		if link[1].xpath('i/@data-icons-before').re("\ue642"):
+    			tagTexts = link[1].xpath('text()').extract()
+    			for tagText in tagTexts:
+    				tagTextAfterStrip = tagText.strip()
+    				if tagTextAfterStrip:
+    					tags.append(tagTextAfterStrip)
     	yield{
     		'pos':pos,
     		'brand_name':brand_name,
     		'url_page':response.css('.account-detail .account-list a.link-nw::attr(href)').extract_first(),
-    		'fans':response.css('.account-detail span.p-nw strong::text').extract_first().strip(),
+    		'fans':"".join(fans.split()),
     		'image_urls':img_url,
+			'tags':tags
     	}
